106c3db2281e42ad2fc6ee23cff4013561d6bfc7
Rahul Sharma <rahulsharmaait@gmail.com>
2016-06-22 18:56:39 -0400

    Added steps to automate creation of tunnel-zones.

--- controller/install4.sh
+++ controller/install4.sh
@@ -5,62 +5,15 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-read -p "Make sure you are running this script only once, else it will break things!!! --> Press ^C (Ctrl + C) to exit; press [Enter] to continue."
+# create tunnel-zone with vxlan
+tunnel_id=$(midonet-cli -A -e tunnel-zone create name tz type vxlan)
 
-# host configuration
-hostname=""
-edge_router_name=""
-
-# floating-ip network
-floating_net=""
-floating_gateway=""
-floating_start=""
-floating_end=""
-
-# uplink configuration
-uplink_name=""
-interface=""
-ip=""
-uplink_net=""
-
-# interface having internet connectivity
-internet_eth=""
-
-# https://docs.midonet.org/docs/latest-en/quick-start-guide/rhel-7_kilo-osp/content/initial_network_configuration.html
-
-source ~/keystonerc_admin
-ext_net_id=$(neutron net-create ext-net --router:external | grep " id " | awk -F' ' '{print $4}')
-edge_router_id=$(neutron router-create $edge_router_name | grep " id " | awk -F' ' '{print $4}')
-pub_subnet_id=$(neutron subnet-create --gateway $floating_gateway --allocation-pool start=$floating_start,end=$floating_end --disable-dhcp $ext_net_id $floating_net | grep " id " | awk -F' ' '{print $4}')
-neutron router-interface-add $edge_router_id $pub_subnet_id
-uplink_net_id=$(neutron net-create $uplink_name --tenant_id admin --provider:network_type uplink | grep " id " | awk -F' ' '{print $4}')
-uplink_subnet_id=$(neutron subnet-create --tenant_id admin --disable-dhcp --name subnetA $uplink_net_id $uplink_net)
-port_id=$(neutron port-create $uplink_net_id --binding:host_id $hostname --binding:profile type=dict interface_name=$interface --fixed-ip ip_address=$ip | grep " id " | awk -F' ' '{print $4}')
-neutron router-interface-add $edge_router_id port=$port_id
-
-# https://docs.midonet.org/docs/latest/operations-guide/content/static_setup.html
-
-ip link add type veth
-ip link set dev veth0 up
-ip link set dev veth1 up
-
-brctl addbr uplinkbridge
-brctl addif uplinkbridge veth0
-ip addr add 172.19.0.1/30 dev uplinkbridge
-ip link set dev uplinkbridge up
-
-sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-
-edge_router=$(midonet-cli -A -e router list | grep $edge_router_name | awk -F' ' '{print $2}')
-port_id=$(midonet-cli -A -e router $edge_router add port address 172.19.0.2 net 172.19.0.0/30)
-midonet-cli -A -e router $edge_router add route src 0.0.0.0/0 dst 0.0.0.0/0 type normal port router $edge_router port $port_id gw 172.19.0.1
-host_id=$(midonet-cli -A -e host list | grep $hostname | awk -F' ' '{print $2}')
-midonet-cli -A -e host $host_id add binding port router $edge_router port $port_id interface veth1
-
-iptables -t nat -I POSTROUTING -o $internet_eth -s $floating_net -j MASQUERADE
-iptables -I FORWARD -s $floating_net -j ACCEPT
-
-service iptables save
-
-echo "Done. Enjoy Midonet!!!"
+# provide the ip-address of each host
+midonet-cli -A -e list host
+read -p "You need to provide private ip-address used by each node for tunneling. Kindly look at the previous output, and based on the UUID and hostname, provide the ip-address. Press [ENTER] to continue..."
+midonet-cli -A -e list host | awk -F' ' '{print $2}' | while read -r host; do
+    echo "Enter ip address of host with UUID: $host followed by [ENTER]"
+    read host_ip
+    midonet-cli -A -e tunnel-zone $tunnel_id add member host $host address $host_ip
+    echo "Added host $host with ip_address $host_ip to the tunnel-zone $tunnel_id"
+done
